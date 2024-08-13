bb55a657c0ccd9ce39b80450d3739b76aacf6d95
LTRSOFT <ltrsoftdev@gmail.com>
2024-08-13 14:25:53 +0530

    main activity changes

--- app/src/main/java/com/ltrsoft/andromotapp/MainActivity.java
+++ app/src/main/java/com/ltrsoft/andromotapp/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Splash_Screen splashScreen=new Splash_Screen();
-        getSupportFragmentManager().beginTransaction().replace(R.id.maincontainer, splashScreen).commit();
+
     }
 }
